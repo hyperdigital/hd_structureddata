51e957a98dbe55cf8533811e1cd464f9d4706cb6
Martin Pribyl <martin.pribyl@hyperdigital.de>
2024-02-02 08:51:43 +0100

    Enabled structured data also for detail pages of news, products, etc

--- Classes/UserFunctions/StructuredDataPrint.php
+++ Classes/UserFunctions/StructuredDataPrint.php
@@ -27,11 +27,18 @@ class StructuredDataPrint
 
     }
 
-    public function printTags()
+    /**
+     * Output the current time in red letters
+     *
+     * @param string          Empty string (no content to process)
+     * @param array           TypoScript configuration
+     * @return        string          HTML output, showing the current server time.
+     */
+    public function printTags(string $content, array $conf)
     {
         $output = [];
 
-        $structuredData = $this->getAllStructuredData();
+        $structuredData = $this->getAllStructuredData($conf);
         foreach ($structuredData as $data) {
             if (!empty($data)) {
                 $output[] = '<script type="application/ld+json">'.json_encode($data).'</script>';
@@ -41,17 +48,39 @@ class StructuredDataPrint
         return implode('', $output);
     }
 
-    public function getAllStructuredData()
+    public function getAllStructuredData($conf)
     {
         $return = [];
-
+        $where = [];
         $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
             ->getQueryBuilderForTable('tx_hdstructureddata_domain_model_structureddata');
 
-        $where = [
-            $queryBuilder->expr()->eq('pid', $queryBuilder->createNamedParameter($this->pid, Connection::PARAM_INT)),
-            $queryBuilder->expr()->eq('sys_language_uid',  $queryBuilder->createNamedParameter($this->sysLanguageUid, Connection::PARAM_INT))
-        ];
+        if (empty($conf)) {
+            $where = [
+                $queryBuilder->expr()->eq('pid', $queryBuilder->createNamedParameter($this->pid, Connection::PARAM_INT)),
+                $queryBuilder->expr()->eq('sys_language_uid',  $queryBuilder->createNamedParameter($this->sysLanguageUid, Connection::PARAM_INT))
+            ];
+        } else {
+            $tableName = $conf['tableName'] ?? 'pages';
+            $fieldName = $conf['fieldName'] ?? 'structured_data';
+            $parentUid = $this->getParentUid($conf['parentUid'] ?? '');
+            $parentUidField = 'foreign_uid';
+            $tableField = 'tablename';
+            $parentField = 'fieldname';
+
+            if (empty($parentUid)) {
+                return '';
+            }
+
+            $where = [
+                $queryBuilder->expr()->eq($tableField,  $queryBuilder->createNamedParameter($tableName)),
+                $queryBuilder->expr()->eq($parentUidField,  $queryBuilder->createNamedParameter($parentUid, Connection::PARAM_INT)),
+                $queryBuilder->expr()->eq('sys_language_uid',  $queryBuilder->createNamedParameter($this->sysLanguageUid, Connection::PARAM_INT))
+            ];
+            if (!empty($parentField)) {
+                $where[] = $queryBuilder->expr()->eq($parentField,  $queryBuilder->createNamedParameter($fieldName));
+            }
+        }
 
         if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('workspaces')) {
             $where[] = $queryBuilder->expr()->eq('t3ver_wsid', $queryBuilder->createNamedParameter(0, Connection::PARAM_INT));
@@ -74,6 +103,23 @@ class StructuredDataPrint
         }
 
         return $return;
+    }
+
+    public function getParentUid($conf)
+    {
+
+        if (substr($conf, 0, 3) == 'GP:') {
+            $parts = GeneralUtility::trimExplode('|', substr($conf, 3));
+            $queryParams = $GLOBALS['TYPO3_REQUEST']->getQueryParams();
+            foreach ($parts as $part) {
+                if (!empty($queryParams[$part])) {
+                    $queryParams = $queryParams[$part];
+                }
+            }
+            return (int) $queryParams;
+        }
+
+        return (int) $conf;
     }
 
     /**
--- README.md
+++ README.md
@@ -19,3 +19,56 @@ Source: https://developers.google.com/search/docs/appearance/structured-data/org
 
 ### Video
 Source: https://developers.google.com/search/docs/appearance/structured-data/video
+
+## Possibility to use it on detail pages (news, products, etc.)
+
+First add a field into database and TCA
+
+_ext_tables.sql_
+```sql
+CREATE TABLE tx_news_domain_model_news (
+    structured_data INT(11) DEFAULT 0 NOT NULL,
+);
+```
+
+_Configuration/TCA/Overrides/tx_news_domain_model_news.php_
+```php 
+$tempColumns = [
+    'structured_data' => [
+        'label' => 'Structured data',
+        'config' => [
+            'type' => 'inline',
+            'foreign_table' => 'tx_hdstructureddata_domain_model_structureddata',
+            'foreign_field' => 'foreign_uid',
+            'foreign_sortby' => 'sorting',
+            'foreign_table_field' => 'tablename',
+            'foreign_match_fields' => [
+                'fieldname' => 'structured_data',
+            ],
+        ],
+    ]
+];
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns("tx_news_domain_model_news", $tempColumns, 1);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+    'tx_news_domain_model_news',
+    '--div--;Structured Data, structured_data',
+);
+```
+
+Then integrate the typoscript code.
+
+_TypoScript_
+```typo3_typoscript
+[request && traverse(request.getQueryParams(), 'tx_news_pi1/news') > 0]
+    # this part is not needed if default typoscript is used
+    # page.headerData.90 = USER
+    # page.headerData.90.userFunc = Hyperdigital\HdStructureddata\UserFunctions\StructuredDataPrint->printTags
+    page.headerData.90 {
+        tableName = tx_news_domain_model_news
+        fieldName = structured_data
+        parentUid = GP:tx_news_pi1|news
+    }
+[end]
+```
